442e48fdc832fdb32c04caee404a1688b9e44939
Parham Abolghasemi <parhamab17@gmail.com>
2023-02-02 18:32:25 +0330

    status cards

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,12 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 // context
 import { GitHubContextProvider } from "../context/GitHubContext";
+// icons
+import { RiGitRepositoryLine, RiUserFollowLine } from "react-icons/ri";
+import { FiUsers } from "react-icons/fi";
+import { BsStickies } from "react-icons/bs";
+import { Avatar, Grid, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
 
 const Info = () => {
   const {
@@ -10,47 +16,74 @@ const Info = () => {
   const items = [
     {
       id: 1,
-      icon: "icon_1",
+      icon: <RiGitRepositoryLine />,
       label: "repos",
       value: public_repos,
-      color: "pink",
+      color: "#006e1878",
     },
     {
       id: 2,
-      icon: "icon_2",
+      icon: <FiUsers />,
       label: "followers",
       value: follower,
-      color: "green",
+      color: "#e7800178",
     },
     {
       id: 3,
-      icon: "icon_3",
+      icon: <RiUserFollowLine />,
       label: "following",
       value: following,
-      color: "blue",
+      color: "#0008ff78",
     },
     {
       id: 4,
-      icon: "icon_4",
+      icon: <BsStickies />,
       label: "gists",
       value: public_gists,
-      color: "yellow",
+      color: "#ff000078",
     },
   ];
 
   return (
-    <div>
-      {items.map(({ id, icon, label, color, value }) => (
-        <section key={id}>
-        
-          <span>{icon}</span>
-          <div>
-            <h3>{value}</h3>
-            <p>{label}</p>
-          </div>
-        </section>
-      ))}
-    </div>
+    <>
+      <Grid container>
+        {items.map(({ id, icon, label, color, value }) => (
+          <React.Fragment key={id}>
+            <Grid item xs={12} lg={4} md={3} display="flex">
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{
+                  background: "#ffffff52",
+                  width: "200px",
+                  p: 1,
+                  borderRadius: 3,
+                  boxShadow:
+                    "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+                }}
+              >
+                <Avatar sx={{ width: 36, height: 36, background: color }}>
+                  {icon}
+                </Avatar>
+                <Stack alignItems="flex-end">
+                  <Typography
+                    variant="subtitle1"
+                    component="h5"
+                    fontWeight={"bold"}
+                  >
+                    {value}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {label}
+                  </Typography>
+                </Stack>
+              </Box>
+            </Grid>
+          </React.Fragment>
+        ))}
+      </Grid>
+    </>
   );
 };
 
