d73f70c3900f9755eef539aa0450d993da937b80
Parham Abolghasemi <parhamab17@gmail.com>
2023-02-07 20:52:41 +0330

    loading component

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,11 +1,10 @@
-import React from 'react';
-
 const Loading = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Loading;
