0cd7feffe055d562687a9f5e587de8bb06e4f506
Parham Abolghasemi <parhamab17@gmail.com>
2023-02-08 10:14:46 +0330

    authentication setup

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,24 @@
 import { Typography } from "@mui/material";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
-  return <div></div>;
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
+    useAuth0();
+
+  return (
+    <>
+      {!isAuthenticated && <button onClick={loginWithRedirect}>Log in</button>}
+      {isAuthenticated && (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          Log out
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { Container } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Auth0
+import { Auth0Provider } from "@auth0/auth0-react";
 // react-router-dom
 import { BrowserRouter } from "react-router-dom";
 // styles
@@ -13,14 +15,20 @@ import GitHubContext from "./context/GitHubContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GitHubContext>
-      <Layout>
-        <Container maxWidth="lg">
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Container>
-      </Layout>
-    </GitHubContext>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <GitHubContext>
+        <Layout>
+          <Container maxWidth="lg">
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </Container>
+        </Layout>
+      </GitHubContext>
+    </Auth0Provider>
   </React.StrictMode>
 );
