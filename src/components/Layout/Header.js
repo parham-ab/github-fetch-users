0e2012399fc717eb1a975171cf521b1c30ebe69f
Parham Abolghasemi <parhamab17@gmail.com>
2023-02-11 13:00:29 +0330

    authentication implemented, bugs fixed

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,10 @@
-import { Typography } from "@mui/material";
+// mui components
+import { Typography, Avatar, Box } from "@mui/material";
+// react-icons
+import { MdOutlineLogout } from "react-icons/md";
+// Auth0
 import { useAuth0 } from "@auth0/auth0-react";
+import { MyBtn } from "../styled-components/Button";
 
 const Header = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
@@ -7,15 +12,44 @@ const Header = () => {
 
   return (
     <>
-      {!isAuthenticated && <button onClick={loginWithRedirect}>Log in</button>}
-      {isAuthenticated && (
-        <button
-          onClick={() => {
-            logout({ returnTo: window.location.origin });
+      {!isAuthenticated && <button onClick={loginWithRedirect}>login</button>}
+      {user && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection={{ xs: "column", sm: "row" }}
+          sx={{
+            background: "#e6efff",
+            padding: "10px",
           }}
         >
-          Log out
-        </button>
+          <Avatar
+            alt={user.name}
+            src={user.picture}
+            sx={{
+              width: "35px",
+              height: "35px",
+            }}
+          />
+          <Box display="flex" pl={1}>
+            <Typography variant="body2">Welcome,</Typography>
+            <Typography variant="body2" fontWeight="bold">
+              &nbsp;{user.name}
+            </Typography>
+          </Box>
+
+          {isAuthenticated && !isLoading && (
+            <MdOutlineLogout
+              style={{
+                fontSize: "20",
+                color: "#cb0000",
+                cursor: "pointer",
+                ml: "6px",
+              }}
+            />
+          )}
+        </Box>
       )}
     </>
   );
--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={window.location.origin}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
   >
     <GitHubContext>
       <Layout>
