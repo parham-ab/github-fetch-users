650e14e42a19d7822a7bbc05d7320a2a197dd104
Parham Abolghasemi <parhamab17@gmail.com>
2023-02-12 20:02:27 +0330

    protect routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 // react-router-dom
 import { Navigate, Route, Routes } from "react-router-dom";
+// Auth0
+import { useAuth0 } from "@auth0/auth0-react";
 // components
-import PrivateRoute from "./components/PrivateRoute";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/" element={isAuthenticated ? <Dashboard /> : <Login />} />
       <Route path="/notfound" element={<NotFound />} />
       <Route path="/login" element={<Login />} />
       <Route path="/*" element={<Navigate to="/notfound" />} />
